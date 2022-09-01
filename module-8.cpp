932f1b64fe716716a49eb55d1f99b9152287e452
osmansufy <osmansufy20@gmail.com>
2022-09-01 14:17:18 +0600

    Done

--- module-8.cpp
+++ module-8.cpp
@@ -35,16 +35,15 @@ public:
             cout << setw(12) << food_item_prices[i] << endl;
         }
     }
-    // Now take some inputs from the user. You need to take the table number of the customer, and the m number of items the customer has taken as input. After that, take all the item's code and quantity as input.
-    // At last calculate the tax amount that is 5% of the total amount. And last show the net total that is the sum of tax and total amount.
 
 public:
     void makeBill()
     {
-        // dynamic array of size m to store the item code and quantity of the food items. You can use this array to calculate the total amount.
         int m;
         int table_number;
+        cout << "Enter the table number: ";
         cin >> table_number;
+        cout << "Enter the number of items: ";
         std::cin >> m;
         string *table_item_names = new string[m];
         int *table_item_code = new int[m];
@@ -53,11 +52,12 @@ public:
         int *table_item_total_price = new int[m];
         for (int i = 0; i < m; i++)
         {
-            cout << "Enter the " << i + 1 << "item code:";
+            cout << "Enter the " << i + 1 << " item code:";
             cin >> table_item_code[i];
-            cout << "Enter the " << i + 1 << "item quantity:";
+            cout << "Enter the " << i + 1 << " item quantity:";
             cin >> table_item_quantity[i];
         }
+
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
@@ -90,6 +90,14 @@ public:
             cout << setw(12) << table_item_total_price[i];
             cout << endl;
         }
+        int total_amount = 0;
+        for (int i = 0; i < m; i++)
+        {
+            total_amount += table_item_total_price[i];
+        }
+        total_tax = total_amount * 0.05;
+        cout << "TAX:" << total_tax << endl;
+        cout << "Total Amount:" << total_amount << endl;
     }
 };
 
